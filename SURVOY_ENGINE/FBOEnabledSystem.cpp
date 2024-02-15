377e42fb96f6062840f8c1755b4a7351819db486
Mishal Zaman <mishalzaman@gmail.com>
2024-02-14 23:30:00 -0500

    update

--- SURVOY_ENGINE/FBOEnabledSystem.cpp
+++ SURVOY_ENGINE/FBOEnabledSystem.cpp
@@ -77,7 +77,7 @@ void ECS::FBOEnabledSystem::Render()
     glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
 
     // make sure we clear the framebuffer's content
-    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClearColor(0.5f, 0.5f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
