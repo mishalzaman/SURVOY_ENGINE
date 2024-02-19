06e2baa1cd60bcfab0eecfff5511807a710df7f3
Mishal Zaman <mishalzaman@gmail.com>
2024-02-19 13:23:06 -0500

    update

--- SURVOY_ENGINE/Scene.cpp
+++ SURVOY_ENGINE/Scene.cpp
@@ -22,7 +22,7 @@ GAME LOOP
 void Scene::Load()
 {
 	//systemManager_->AddSystem<ECS::PhysicsSystem>(*entityManager_, *physics_, *eventManager_);
-	//systemManager_->AddSystem<ECS::FBOBeginSystem>(*eventManager_);
+	//systemManager_->AddSystem<ECS::FBOBeginSystem>(*eventManager_); 
 	//systemManager_->AddSystem<ECS::CharacterControllerSystem>(*entityManager_, *physics_, *eventManager_);
 	//systemManager_->AddSystem<ECS::CameraThirdPersonSystem>(*entityManager_, *physics_, *eventManager_);
 	//systemManager_->AddSystem<ECS::CameraFreeLookSystem>(*entityManager_, *physics_, *eventManager_);
@@ -35,9 +35,9 @@ void Scene::Load()
 	systemManager_->AddSystem<ECS::CameraFreeLookSystem>(*entityManager_, *physics_, *eventManager_);
 	systemManager_->AddSystem<ECS::MeshStaticBuffersSystem>(*entityManager_);
 	systemManager_->AddSystem<ECS::RenderPassDepthMapSystem>(*entityManager_);
-	//systemManager_->AddSystem<ECS::FBOBeginSystem>(*eventManager_);
+	systemManager_->AddSystem<ECS::FBOBeginSystem>(*eventManager_);
 	systemManager_->AddSystem<ECS::RenderStatic3DSystem>(*entityManager_, *physics_, *eventManager_);
-	//systemManager_->AddSystem<ECS::FBORendererSystem>(*eventManager_);
+	systemManager_->AddSystem<ECS::FBORendererSystem>(*eventManager_);
 
 	systemManager_->Load();
 }
