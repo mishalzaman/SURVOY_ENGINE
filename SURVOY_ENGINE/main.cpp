8572ca3fc478bb978970d48b54b86b23f9ee6adf
Mishal Zaman <mishalzaman@gmail.com>
2024-02-27 16:19:44 -0500

    update

--- SURVOY_ENGINE/main.cpp
+++ SURVOY_ENGINE/main.cpp
@@ -6,11 +6,11 @@
 #include "KeyPressEvent.h"
 #include "SwitchCameraEvent.h"
 
-std::string version = "Basic Ass Engine 0.2.82";
+std::string version = "Basic Ass Engine 0.2.83";
 
 /*
 		  *--------------------------*
-		     Current version: 0.2.82
+		     Current version: 0.2.83
 		  *--------------------------*
 
 DEVELOPMENT NOTES
